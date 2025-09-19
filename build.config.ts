f98c439530e9d84f2fa0a05db33e75ec74ce2c6c
kspace-trk <keigonakao.0825@gmail.com>
2025-09-19 16:21:17 +0900

    feat: indexをmjsで出力するように

--- build.config.ts
+++ build.config.ts
@@ -2,4 +2,15 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   failOnWarn: false,
+  entries: [
+    // メインモジュール
+    'src/module',
+    // ランタイムファイルを.mjsとして出力
+    {
+      input: 'src/runtime/',
+      outDir: 'dist/runtime',
+      format: 'esm',
+      ext: 'mjs',
+    },
+  ],
 })
---
+++ dist/runtime/index.mjs
@@ -0,0 +1,11 @@
+export { default as KSMainButton } from "./components/buttons/MainButton.vue";
+export { default as KSInputField } from "./components/forms/InputField.vue";
+export { default as KSTextareaField } from "./components/forms/TextareaField.vue";
+export { default as KSTopHeader } from "./components/headers/TopHeader.vue";
+export { default as KSSideHeader } from "./components/headers/SideHeader.vue";
+export { default as KSTextItem } from "./components/panels/TextItem.vue";
+export { default as KSSectionTextWithLine } from "./components/sections/SectionTextWithLine.vue";
+export { default as KSDashboardContainer } from "./components/layouts/DashboardContainer.vue";
+export * from "./types/index.mjs";
+export const CSS_RESET = "./assets/css/reset.css";
+export const CSS_FONTS = "./assets/css/fonts/mplus-1p.css";
